1465f7120b86ef128f3fdf5595f5905875ad6d68
Hinoki Crum <hinokicrum@gmail.com>
2023-08-24 17:42:02 -0700

    initial commit

--- num_eval.py
+++ num_eval.py
@@ -1,10 +1,11 @@
 import json
+from transformers import pipeline
 
 f = open('DryRun_Numerical_Reasoning.json')
 
 data = json.load(f)
 
-print(data[0])
+print(data[0]['masked headline'].replace('____', '<mask> '))
 print("")
 print(data[1])
 print("")
@@ -12,5 +13,11 @@ print(data[2])
 
 #for i in data:
 #    print(i)
-
 f.close()
+
+text = data[0]['masked headline'].replace('____', '<mask> ')
+
+mask_filler = pipeline("fill-mask", "stevhliu/my_awesome_eli5_mlm_model")
+
+print("")
+print(mask_filler(text, top_k=3))
