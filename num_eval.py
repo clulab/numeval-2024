054aa1d8ef82d4b09a82973385a05081c4f59337
Hinoki Crum <hinokicrum@gmail.com>
2023-08-26 14:50:22 -0700

    finetuned distillroberta

--- num_eval.py
+++ num_eval.py
@@ -1,23 +1,108 @@
-import json
-from transformers import pipeline
+import pandas as pd
+import tensorflow as tf
+from transformers import AutoTokenizer
+import re
+from transformers import DataCollatorForLanguageModeling
+from transformers import AdamWeightDecay
+from transformers import TFAutoModelForMaskedLM
+from datasets import Dataset
+import math
+
 
 f = open('DryRun_Numerical_Reasoning.json')
-
-data = json.load(f)
-
-print(data[0]['masked headline'].replace('____', '<mask> '))
-print("")
-print(data[1])
-print("")
-print(data[2])
-
-#for i in data:
-#    print(i)
+df = pd.read_json(f)
+print(df.info())
+df['news'] = df['news'].apply(lambda x: re.sub(r'\([^)]*\)', '', x))
+df['masked headline'] = df['masked headline'].str.replace('____', '<mask> ')
+df['text'] = df[['news', 'masked headline']].apply(" ".join, axis=1)
+print(df['text'].head())
+df['ans']=df['ans'].str.replace(',','')
 f.close()
 
-text = data[0]['masked headline'].replace('____', '<mask> ')
+dataset = Dataset.from_pandas(df)
+dataset = dataset.train_test_split(test_size=0.2)
 
-mask_filler = pipeline("fill-mask", "stevhliu/my_awesome_eli5_mlm_model")
+def preprocess_function(examples):
+    return tokenizer([" ".join(x) for x in examples['text']])
 
-print("")
-print(mask_filler(text, top_k=3))
+tokenizer = AutoTokenizer.from_pretrained("distilroberta-base")
+tokenized = dataset.map(preprocess_function, batched=True, num_proc=4,
+                        remove_columns=dataset["train"].column_names)
+
+block_size = 128
+
+def group_texts(examples):
+    # Concatenate all texts.
+    concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+    total_length = len(concatenated_examples[list(examples.keys())[0]])
+    # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
+    # customize this part to your needs.
+    if total_length >= block_size:
+        total_length = (total_length // block_size) * block_size
+    # Split by chunks of block_size.
+    result = {
+        k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+        for k, t in concatenated_examples.items()
+    }
+    result["labels"] = result["input_ids"].copy()
+    return result
+
+lm_dataset = tokenized.map(group_texts,  batched=True, num_proc=4)
+
+data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, return_tensors="tf")
+
+optimizer = AdamWeightDecay(learning_rate=2e-5, weight_decay_rate=0.01)
+
+model = TFAutoModelForMaskedLM.from_pretrained("distilroberta-base")
+
+tf_train_set = model.prepare_tf_dataset(
+    lm_dataset["train"],
+    shuffle=True,
+    batch_size=16,
+    collate_fn=data_collator,
+)
+
+tf_test_set = model.prepare_tf_dataset(
+    lm_dataset["test"],
+    shuffle=False,
+    batch_size=16,
+    collate_fn=data_collator,
+)
+
+model.compile(optimizer=optimizer)  # No loss argument!
+
+eval_loss = model.evaluate(tf_test_set)
+print(f"Pretrained LM Perplexity: {math.exp(eval_loss):.2f}")
+
+'''
+checkpoint_path = "training_1/cp.ckpt"
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+'''
+
+model.fit(x=tf_train_set, epochs=5)
+
+eval_loss = model.evaluate(tf_test_set)
+print(f"Finetuned Perplexity: {math.exp(eval_loss):.2f}")
+
+test_sentence = dataset['test']['masked headline'][0]
+inputs = tokenizer(test_sentence, return_tensors="tf")
+mask_token_index = tf.where(inputs["input_ids"] == tokenizer.mask_token_id)[0, 1]
+logits = model(**inputs).logits
+mask_token_logits = logits[0, mask_token_index, :]
+top_3_tokens = tf.math.top_k(mask_token_logits, 3).indices.numpy()
+
+for token in top_3_tokens:
+    print(test_sentence.replace(tokenizer.mask_token, tokenizer.decode([token])))
+print("Answer: ", dataset['test']['ans'][0])
+
+'''
+mask_filler = pipeline(
+    "fill-mask", model="training_1/cp.ckpt")
+
+print(dataset['test']['masked headline'][0])
+print(mask_filler("The most common household pets are <mask> and dogs.", top_k=1))
+
+print(mask_filler(dataset['test']['masked headline'][0], top_k=3))
+'''
