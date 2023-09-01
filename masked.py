4be50d0cdc738a5ba194f707d0cfde948dea5687
Hinoki Crum <hinokicrum@gmail.com>
2023-09-01 16:24:59 -0700

    updated

--- masked.py
+++ masked.py
@@ -23,11 +23,14 @@ f.close()
 dataset = Dataset.from_pandas(df)
 dataset = dataset.train_test_split(test_size=0.2)
 
+model_name = "distilroberta-base"
+#model_name = "distilbert-base-uncased"
+
 def preprocess_function(examples):
     #return tokenizer([" ".join(x) for x in examples['text']]) # not join
     return tokenizer(examples['text'])
 
-tokenizer = AutoTokenizer.from_pretrained("distilroberta-base", use_fast=False)
+tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False)
 tokenized = dataset.map(preprocess_function, batched=True, num_proc=4,
                         remove_columns=dataset["train"].column_names)
 
@@ -55,7 +58,7 @@ data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, return_tens
 
 optimizer = AdamWeightDecay(learning_rate=2e-5, weight_decay_rate=0.01)
 
-model = TFAutoModelForMaskedLM.from_pretrained("distilroberta-base")
+model = TFAutoModelForMaskedLM.from_pretrained(model_name)
 
 '''
 tf_train_set = model.prepare_tf_dataset(
@@ -98,7 +101,7 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
                                                  verbose=1)
 '''
 
-model.fit(x=tf_train_set, epochs=15)
+model.fit(x=tf_train_set, epochs=10)
 
 eval_loss = model.evaluate(tf_test_set)
 print(f"Finetuned Perplexity: {math.exp(eval_loss):.2f}")
@@ -127,6 +130,7 @@ print(mask_filler("The most common household pets are <mask> and dogs.", top_k=1
 
 print(mask_filler(dataset['test']['masked headline'][0], top_k=3))
 '''
+'''
 for i in range(len(dataset['test'])):
     inputs = tokenizer(dataset['test']['text'][i], return_tensors="tf")
     mask_token_index = tf.where(inputs["input_ids"] == tokenizer.mask_token_id)[0, 1]
@@ -137,3 +141,14 @@ for i in range(len(dataset['test'])):
     print("Prediction: ", prediction)
     print("Answer: ", dataset['test']['ans'][i])
     print(prediction == dataset['test']['ans'][i])
+'''
+for i in dataset['test']:
+    inputs = tokenizer(i['text'], return_tensors="tf")
+    mask_token_index = tf.where(inputs["input_ids"] == tokenizer.mask_token_id)[0, 1]
+    logits = model(**inputs).logits
+    mask_token_logits = logits[0, mask_token_index, :]
+    top_token = tf.math.top_k(mask_token_logits, 1).indices.numpy()
+    prediction = tokenizer.decode([top_token][0]).strip()
+    print("Prediction: ", prediction)
+    print("Answer: ", i['ans'])
+    print(prediction == i['ans'])
